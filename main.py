3e6ae0e5e8c365b3eb681f971593b0952e5295cf
Leonardo Fernandes Alonso <38121499+DevLeonardoFA@users.noreply.github.com>
2021-12-24 20:21:13 +0000

    eu pensei em ir mais devagar, e tirei a parte de ler outro arquivo e o sistema de rotas, deixei o código do dado e melhorei

--- main.py
+++ main.py
@@ -7,32 +7,61 @@ from flask import Flask
 app = Flask(__name__)
 
 # começo da brincadeira
-class d20:
+class dado:
   def __init__(self):
     self.valor_min = 0
-    self.valor_max = 20
-    self.frase = "quer jogar o d20?\n"
+    self.frase = "Qual dado quer jogar?\n\n[0] - d2 \n[1] - d4 \n[2] - d6 \n[3] - d8 \n[4] - d10 \n[5] - d20 \n\nescolha: "
+    self.jogadas = 1
 
   def Iniciar(self):
-    resposta = input(self.frase)
-    if(resposta == 'sim' or resposta == 's'):
-      self.Valor()
-    else:
-      print('thks')
-      
-  def Valor(self):
-    print(random.randint(self.valor_min, self.valor_max))
 
-dado = d20()
-# dado.Iniciar()
+    while self.jogadas == 1 :
+      
+      resposta = input(self.frase)
+
+      if(resposta == '0' ):
+        self.Valor(2)
+      else:
+        if(resposta == '1' ):
+          self.Valor(4)
+        else:
+          if(resposta == '2' ):
+            self.Valor(6)
+          else:
+            if(resposta == '3' ):
+              self.Valor(8)
+            else:
+              if(resposta == '4' ):
+                self.Valor(10)
+              else:
+                if(resposta == '5' ):
+                  self.Valor(20)
+                else:
+                  print('erro');
+
+
+  def Valor(self, max):
+    resultado = random.randint(self.valor_min, max)
+    print('\nresultado do dado: ', resultado, '\n\n')
+
+    jogar_novamente = input('quer jogar novamente? [s] [n] \n\nresposta: ')
+    print('\n')
+    if(jogar_novamente == 'sim' or jogar_novamente == 's'):
+      self.jogadas = 1;
+    else:
+      self.jogadas = 0;
+
+
+dado = dado()
+dado.Iniciar()
 
 # abrindo um outro arquivo
-home = codecs.open("test.html", 'r')
+# home = codecs.open("test.html", 'r')
 
-# criando rota
-@app.route('/')
-def front():
-  return home.read()
+# # criando rota
+# @app.route('/')
+# def front():
+#   return home.read()
     
-# executando servidor
-app.run(host='0.0.0.0')
+# # executando servidor
+# app.run(host='0.0.0.0')
